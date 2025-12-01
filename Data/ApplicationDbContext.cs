de6d7b1b88e0378a54d6ad8b9a53b5b4b9f40c38
Jesus Beltran <a2233336114@alumnos.uat.edu.mx>
2025-11-30 21:16:19 -0600

    Add ApplicationDbContext for task management

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using TaskManager.Models;
+
+namespace TaskManager.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TaskItem> Tasks { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<TaskItem>()
+                .HasOne(t => t.User)
+                .WithMany(u => u.Tasks)
+                .HasForeignKey(t => t.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
